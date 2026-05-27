c45bbe04fd70a8e82fd5b3d09c80709bf0963d3c
UdanR <24432ur@hvhs.school.nz>
2026-05-27 10:31:25 +0000

    May 27, 2026, 10:31 AM

--- script.js
+++ script.js
@@ -31,11 +31,12 @@ var Desserts = foodItems.filter(product => product.category === 'Desserts');
 
 console.log(Desserts);
 
-const Recommended_list = document.getElementById("Recommended_items");
+const RECOMMNEDED_ITEMS_OUTPUT = document.getElementById("Recommended_items");
 var Recommend = foodItems.filter(product => product.recommend === true);
 console.log(Recommend);
 
 Recommend.forEach(Recommended_print);
 
 function Recommended_print(){
+    RECOMMNEDED_ITEMS_OUTPUT.innerHTML =
 }
