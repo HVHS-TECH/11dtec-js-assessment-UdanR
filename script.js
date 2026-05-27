59e907aef087c911e7120eb81035afb0d8699788
UdanR <24432ur@hvhs.school.nz>
2026-05-27 09:20:00 +0000

    May 27, 2026, 9:20 AM

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Hello world!")
 var foodItems = [
-    {Id: "101", Catogary}
-]
+  { id: 101, item: 'orange', price: '$6', img: 'orange.png' },
+  { id: 102, item: 'apple',  price: '$5', img: 'apple.png' }
+];
