cbabd3275a2ae89b498655e3a41a004638c8a3fb
UdanR <24432ur@hvhs.school.nz>
2026-05-27 09:34:31 +0000

    May 27, 2026, 9:34 AM

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log("Hello world!")
 var foodItems = [
-  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png' },
-  { id: 102, category: 'main-dish' ,item: 'BBQ Bacon Burger',  price: '$15', img: 'BBQ Bacon Burger.png' }
+  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png'},
+  { id: 102, category: 'main-dish' ,item: 'BBQ Bacon Burger',  price: '$15', img: 'BBQ Bacon Burger.png'},
+  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png'},
+  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png'},
+  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png'},
+  { id: 101, category: 'main-dish' ,item: 'Classic Burger', price: '$12', img: 'Classic Burger.png'},
+
 ];
