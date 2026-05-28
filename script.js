fb286c17110123fbff3b48f163ab4ed744e98ddb
UdanR <24432ur@hvhs.school.nz>
2026-05-28 00:45:06 +0000

    May 28, 2026, 12:45 AM

--- script.js
+++ script.js
@@ -63,4 +63,4 @@ function Add_To_Cart_Function (Button_ID) {
 
 // Adding the total number next to the Shopping cart icon
 const CART_TOTAL_PRICE = document.getElementById("Shopping-cart-price");
-CART_TOTAL_PRICE.innerHTML = Total_Price;
+CART_TOTAL_PRICE.innerHTML = "$ " + Total_Price;
