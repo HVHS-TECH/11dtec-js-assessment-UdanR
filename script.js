869bef6723c3410b7645cefa46adbeb0719d14a8
UdanR <24432ur@hvhs.school.nz>
2026-05-28 01:01:00 +0000

    May 28, 2026, 1:01 AM

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ var Desserts = foodItems.filter(product => product.category === 'Desserts');
 console.log(Desserts);
 
 // Recommended Food area
-const RECOMMNEDED_ITEMS_OUTPUT = document.getElementById("Recommended_items_containor");
+const RECOMMNEDED_ITEMS_OUTPUT = document.getElementById("Recommended_items_containor_output");
 var Recommend = foodItems.filter(product => product.recommend === true);
 console.log(Recommend);
 
 Recommend.forEach(Recommended_print);
 
-var reco_Item_Num = 1;
+
 function Recommended_print(item){
-        reco_Item_Num = reco_Item_Num + 1;
-        console.log (`<div class="${reco_Item_Num}">
+    var reco_Item_Num = 1;
+        console.log (`<div class="_${reco_Item_Num}">
                 <img src="Images/${item.img}" alt="${item.img}">
                 <div class="item_info">
                     <h2 class="item_Name">${item.item}</h2>
@@ -49,7 +49,7 @@ function Recommended_print(item){
                 </div>
                 <button value=${item.id} onclick="Adding price to kart" class=" Add_To_Cart_Button">Add To Cart</button>
             </div>`);
-    RECOMMNEDED_ITEMS_OUTPUT.innerHTML += `<div class="${reco_Item_Num}">
+    RECOMMNEDED_ITEMS_OUTPUT.innerHTML += `<div class="_${reco_Item_Num}">
                 <img src="Images/${item.img}" alt="${item.img}">
                 <div class="item_info">
                     <h2 class="item_Name">${item.item}</h2>
@@ -57,7 +57,7 @@ function Recommended_print(item){
                 </div>
                 <button value=${item.id} onclick="Adding price to kart" class=" Add_To_Cart_Button">Add To Cart</button>
             </div>`
-
+            reco_Item_Num = reco_Item_Num + 1;
 }
 
 
