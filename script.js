316f9cba3a8df157b1efcd01003e749105a75546
UdanR <24432ur@hvhs.school.nz>
2026-05-29 13:34:02 +0000

    May 29, 2026, 1:34 PM

--- script.js
+++ script.js
@@ -81,6 +81,32 @@ for (let i = 0; i < foodItems.length; i++ ){
             
 }
 
+function Menu_Item_Selecting_All (){
+    CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML = "";
+
+
+    for (let i = 0; i < foodItems.length; i++ ){
+        var thisitem = foodItems[i];
+        console.log (`<div class="itme-${i}">
+        <img src="Items_IMG/${thisitem.img}" alt="${thisitem.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${thisitem.item}</h2>
+            <h2 class="item_Price">$ ${thisitem.price}</h2>
+        </div>
+        <button value=${thisitem.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`);
+        CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML += `<div class="_${i}">
+        <img src="Items_IMG/${thisitem.img}" alt="${thisitem.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${thisitem.item}</h2>
+            <h2 class="item_Price">$ ${thisitem.price}</h2>
+        </div>
+        <button value=${thisitem.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`
+            
+}
+}
+
 function Menu_Item_Selecting_Main_Dished (){
     CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML = "";
 
@@ -112,6 +138,60 @@ function Menu_Item_Selecting_Sides (){
     CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML = "";
 
     var MainDish = foodItems.filter(product => product.category === 'Sides');
+    MainDish.forEach(MainDish_Print);
+    var Item_Class_NUM = 0;
+    function MainDish_Print(item, Item_Class_NUM){
+        console.log (`<div class="itme-${Item_Class_NUM}">
+        <img src="Items_IMG/${item.img}" alt="${item.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${item.item}</h2>
+            <h2 class="item_Price">$ ${item.price}</h2>
+        </div>
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`);
+        CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML += `<div class="_${Item_Class_NUM}">
+        <img src="Items_IMG/${item.img}" alt="${item.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${item.item}</h2>
+            <h2 class="item_Price">$ ${item.price}</h2>
+        </div>
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`
+        Item_Class_NUM = Item_Class_NUM + 1;
+    }
+}
+
+function Menu_Item_Selecting_Drinks (){
+    CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML = "";
+
+    var MainDish = foodItems.filter(product => product.category === 'Drinks');
+    MainDish.forEach(MainDish_Print);
+    var Item_Class_NUM = 0;
+    function MainDish_Print(item, Item_Class_NUM){
+        console.log (`<div class="itme-${Item_Class_NUM}">
+        <img src="Items_IMG/${item.img}" alt="${item.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${item.item}</h2>
+            <h2 class="item_Price">$ ${item.price}</h2>
+        </div>
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`);
+        CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML += `<div class="_${Item_Class_NUM}">
+        <img src="Items_IMG/${item.img}" alt="${item.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${item.item}</h2>
+            <h2 class="item_Price">$ ${item.price}</h2>
+        </div>
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+        </div>`
+        Item_Class_NUM = Item_Class_NUM + 1;
+    }
+}
+
+function Menu_Item_Selecting_Desserts (){
+    CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML = "";
+
+    var MainDish = foodItems.filter(product => product.category === 'Desserts');
     MainDish.forEach(MainDish_Print);
     var Item_Class_NUM = 0;
     function MainDish_Print(item, Item_Class_NUM){
