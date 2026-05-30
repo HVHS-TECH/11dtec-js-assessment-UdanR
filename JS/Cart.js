6bf98f95ac3be3523879342abeebaf44039629e7
UdanR <24432ur@hvhs.school.nz>
2026-05-30 07:49:27 +0000

    May 30, 2026, 7:49 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -1,6 +1,6 @@
 // The Inventory
 console.log(JSON.parse(sessionStorage.getItem('Inventory')));
-
+var foodItems = JSON.parse(sessionStorage.getItem('Inventory'));
 
 // Getting the buying_Items stringed var form the sessionStorage and coverting it back into a js
 console.log(JSON.parse(sessionStorage.getItem('buying_Items')));
@@ -9,5 +9,6 @@ console.log(added_Buying_Items);
 console.log(foodItems);
 for (let i = 0; i < added_Buying_Items.length; i++) {
     console.log("hello World!");
+    
 
 }
