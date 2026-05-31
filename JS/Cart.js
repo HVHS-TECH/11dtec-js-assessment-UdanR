277016f0346fdb3a74250fb11a34d56e8d9d5df7
UdanR <24432ur@hvhs.school.nz>
2026-05-31 00:31:50 +0000

    May 31, 2026, 12:31 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -7,6 +7,8 @@ console.log(JSON.parse(sessionStorage.getItem('buying_Items')));
 var added_Buying_Items = (JSON.parse(sessionStorage.getItem('buying_Items')));
 console.log(added_Buying_Items);
 console.log(foodItems);
+
+// Adding the items to the HTML
 for (let i = 0; i < added_Buying_Items.length; i++) {
     console.log("hello World!");
     var buying_items = added_Buying_Items[i];
@@ -24,24 +26,32 @@ for (let i = 0; i < added_Buying_Items.length; i++) {
                         <h2 class="item_Price">$${item.price}</h2>
                     </div>
                     <div class="Item_Quantity">
-                        <input min="1" type="number" value="${buying_items.q}" id="item_quantity_${item.id}" class="item_quantity">
+                        <input min="1" type="number" value="${buying_items.quantity}" id="item_quantity_${item.id}" class="item_quantity">
                     </div>
                     <div class="Item_Total">
                         <button value="${item.id}" onclick="Remove_From_Cart_Function(this.value)" class="Remove_From_Cart_Button">Remove</button>
                         <h2 class="Chosen_Item_Total" id="chosen_Item_Total_Price">$10</h2>
                     </div>
-                </div></div>`);
-        RECOMMNEDED_ITEMS_OUTPUT.innerHTML += `<div class="_${reco_Item_Num}">
-                <img src="Items_IMG/${item.img}" alt="${item.img}">
-                <div class="item_info">
-                    <h2 class="item_Name">${item.item}</h2>
-                    <h2 class="item_Price" id="item_Price_${item.id}">$ ${item.price}</h2>
                 </div>
-                <div class="Submit_Info">
-                <input type="number" value=1 class="item_quantity_choosing" id="item_quantity_${item.id}">
-                <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button">Add To Cart</button>
+                </div>`);
+        RECOMMNEDED_ITEMS_OUTPUT.innerHTML += `<div class="item-${Item_Num}">                
+                <div class="chosen_Item_IMG">
+                    <img src="Items_IMG/${item.img}" alt="${item.img}">
                 </div>
-            </div>`
+                <div class="chosen_Item_Info">
+                    <div class="Item_Details">
+                        <h2 class="item_Name">${item.item}</h2>
+                        <h2 class="item_Price">$${item.price}</h2>
+                    </div>
+                    <div class="Item_Quantity">
+                        <input min="1" type="number" value="${buying_items.quantity}" id="item_quantity_${item.id}" class="item_quantity">
+                    </div>
+                    <div class="Item_Total">
+                        <button value="${item.id}" onclick="Remove_From_Cart_Function(this.value)" class="Remove_From_Cart_Button">Remove</button>
+                        <h2 class="Chosen_Item_Total" id="chosen_Item_Total_Price">$10</h2>
+                    </div>
+                </div>
+                </div>`;
             Item_Num = Item_Num +1;
 }
 }
