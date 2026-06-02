c630a34cc7e9c8b5976a60400a7e35628fbd3a09
UdanR <24432ur@hvhs.school.nz>
2026-06-02 03:14:00 +0000

    Jun 2, 2026, 3:14 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -36,5 +36,23 @@ for (let i = 0; i < added_Buying_Items.length; i++) {
                     </div>
                 </div>
                 </div>`);
+                choosen_Cart_Output.inner (`<div class="item-${Item_Num}">                
+                <div class="chosen_Item_IMG">
+                    <img src="Items_IMG/${choice.img}" alt="${choice.img}">
+                </div>
+                <div class="chosen_Item_Info">
+                    <div class="Item_Details">
+                        <h2 class="item_Name">${choice.item}</h2>
+                        <h2 class="item_Price">$${choice.price}</h2>
+                    </div>
+                    <div class="Item_Quantity">
+                        <input min="1" type="number" value="${buying_items.quantity}" id="item_quantity_${choice.id}" class="item_quantity">
+                    </div>
+                    <div class="Item_Total">
+                        <button value="${choice.id}" onclick="Remove_From_Cart_Function(this.value)" class="Remove_From_Cart_Button">Remove</button>
+                        <h2 class="Chosen_Item_Total" id="chosen_Item_Total_Price">$10</h2>
+                    </div>
+                </div>
+                </div>`);
 }
 
