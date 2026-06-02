09caa02b5e48792f3d28078f42ffa4b0085d28a3
UdanR <24432ur@hvhs.school.nz>
2026-06-02 03:05:07 +0000

    Jun 2, 2026, 3:05 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -13,6 +13,6 @@ const CART_ITEM_OUTPUT = document.getElementById("choosen_Cart_Output");
 // Adding the items to the HTML
 for (let i = 0; i < added_Buying_Items.length; i++) {
     var buying_items = added_Buying_Items[i];
-    console.log()
+    console.log(buying_items)
 }
 
