cd8eb915cf5765e86b2d946dd0eebbd91b1a0e82
UdanR <24432ur@hvhs.school.nz>
2026-06-02 02:30:28 +0000

    Jun 2, 2026, 2:30 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -18,6 +18,7 @@ for (let i = 0; i < added_Buying_Items.length; i++) {
     choice.forEach(choice_Print);
     var Item_Num = 1;
     function choice_Print(item, Item_Num){
+        console.log("hi")
         console.log (`<div class="item-${Item_Num}">                
                 <div class="chosen_Item_IMG">
                     <img src="Items_IMG/${item.img}" alt="${item.img}">
