217d1569c049f15e1bb932f6a5b2c0d106dd0199
UdanR <24432ur@hvhs.school.nz>
2026-06-09 23:14:24 +0000

    Jun 9, 2026, 11:14 PM

--- JS/Cart.js
+++ JS/Cart.js
@@ -63,7 +63,7 @@ for (let i = 0; i < added_Buying_Items.length; i++) {
                 function changing_Item_Quantity(){
                     const item_quantity = document.getElementById(`item_quantity_${choice.id}`).value;
                     console.log(choice.id +": "+item_quantity);
-                    var excitingItem = added_Buying_Items.find(product => product.id === Button_ID);
+                    var excitingItem = added_Buying_Items.find(product => product.id === choice.id);
                     console.log(excitingItem);
                         if (excitingItem){
                         excitingItem.quantity += Quantity;
