698bea505eeca706c3b9e027f810c10a995d5ec3
UdanR <24432ur@hvhs.school.nz>
2026-06-13 05:54:15 +0000

    Jun 13, 2026, 5:54 AM

--- JS/Cart.js
+++ JS/Cart.js
@@ -112,6 +112,8 @@ for (let i = 0; i < added_Buying_Items.length; i++) {
 
 }
 
+var updating_quantity_input_box = changing_Item_Quantity();
+
 //adding the total price next to the cart
 const CART_TOTAL_PRICE = document.getElementById("Shopping-cart-price");
 CART_TOTAL_PRICE.innerHTML = "$" + Total_Price;
