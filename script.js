fd23f95e1c6649b99a46353633328818769596d9
UdanR <24432ur@hvhs.school.nz>
2026-06-19 02:51:45 +0000

    Jun 19, 2026, 2:51 AM

--- script.js
+++ script.js
@@ -197,15 +197,18 @@ function Menu_Item_Selecting_Desserts (){
 
 // Cart Arrays
 var Shopping_Bag = [];
-// Adding the total number next to the Shopping cart icon
+
+// Checking if their is a number saved if so adding it next to the shopping cart if not nothing
 var Total_Price = 0;
     // Saving total price
     sessionStorage.setItem('Total_price', JSON.stringify(Total_Price));
     console.log(JSON.parse(sessionStorage.getItem('Total_price')));
-    
+
+    const CART_TOTAL_PRICE = document.getElementById("Shopping-cart-price");
+
     console.log(JSON.parse(sessionStorage.getItem('Total_price')));
     var Total_Price_Check = (JSON.parse(sessionStorage.getItem('Total_price')));
-    if (Total_Price_Check < 0){
+    if (Total_Price_Check === 0){
         console.log ("it's null")
     } else {
         console.log("it's not null")
@@ -250,7 +253,6 @@ function Add_To_Cart_Function (Button_ID) {
 
 
 // Adding the total number next to the Shopping cart icon
-var Total_Price = 0;
 
 for (let i = 0; i < Shopping_Bag.length; i++){
     let price_adding_item_object = Shopping_Bag[i];
