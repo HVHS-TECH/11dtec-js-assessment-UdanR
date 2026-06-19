af36816b95102849799f666dd4a08524ba88f86d
UdanR <24432ur@hvhs.school.nz>
2026-06-19 02:41:48 +0000

    Jun 19, 2026, 2:41 AM

--- script.js
+++ script.js
@@ -199,10 +199,8 @@ function Menu_Item_Selecting_Desserts (){
 var Shopping_Bag = [];
 
 
-// Adding the total number next to the Shopping cart icon
-var Total_Price = 0;
-sessionStorage.setItem('Total_price', JSON.stringify(Total_Price));
-console.log(JSON.parse(sessionStorage.getItem('Total_price')));
+    console.log(JSON.parse(sessionStorage.getItem('Total_price')));
+
 
 // Adding items to carts
 function Add_To_Cart_Function (Button_ID) {
@@ -240,6 +238,8 @@ function Add_To_Cart_Function (Button_ID) {
 
 
 // Adding the total number next to the Shopping cart icon
+var Total_Price = 0;
+
 const CART_TOTAL_PRICE = document.getElementById("Shopping-cart-price");
 for (let i = 0; i < Shopping_Bag.length; i++){
     let price_adding_item_object = Shopping_Bag[i];
@@ -257,6 +257,9 @@ for (let i = 0; i < Shopping_Bag.length; i++){
     // Converting the Shopping_Bag Var to a string and storing it in a seesionStorgae
     sessionStorage.setItem('buying_Items', JSON.stringify(Shopping_Bag));
     console.log(JSON.parse(sessionStorage.getItem('buying_Items')));
+    // Saving total price
+    sessionStorage.setItem('Total_price', JSON.stringify(Total_Price));
+    console.log(JSON.parse(sessionStorage.getItem('Total_price')));
 }
 }
 
