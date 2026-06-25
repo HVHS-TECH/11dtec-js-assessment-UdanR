12c836df4903185bf29fcdf9128af272ea474026
UdanR <24432ur@hvhs.school.nz>
2026-06-25 10:00:50 +0000

    Jun 25, 2026, 10:00 AM

--- script.js
+++ script.js
@@ -217,9 +217,15 @@ function Menu_Item_Selecting_Desserts (){
     }
 }
 
+const MAXIMUM_ORDERING_NUM = 100;
+//======================================================WORKING IN PROGRESS===========================================
 // When a number is bigger than 100 it is converted into a 100
 function number_Cramping(value) {
+    //Getting the inputbox
     const ITEM_QUENTITY_NUM_CRAMPING = document.getElementById("item_quantity_"+value);
+    //Finding the item in the shopping bag
+    var item_Quantity_Num_Check = Shopping_Bag.find(product => product.id === value);
+    var item_Clamping_Num_Calc = MAXIMUM_ORDERING_NUM - item_Quantity_Num_Check.quantity;
     const ITEM_NUM_CRAMPING = Math.min(ITEM_QUENTITY_NUM_CRAMPING.value, 100);
     document.getElementById("item_quantity_"+value).value = ITEM_NUM_CRAMPING;
     item_quantity = ITEM_NUM_CRAMPING;
