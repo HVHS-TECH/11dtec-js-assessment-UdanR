9be66194ed367aeb80f0f0771dc4b61de09bbd75
UdanR <24432ur@hvhs.school.nz>
2026-07-22 00:41:02 +0000

    Jul 22, 2026, 12:41 AM

--- script.js
+++ script.js
@@ -290,7 +290,22 @@ function Menu_Item_Selecting_Desserts (){
     MainDish.forEach(MainDish_Print);
     var Item_Class_NUM = 0;
     function MainDish_Print(item, Item_Class_NUM){
-        if (MainDish === true){}
+        if (MainDish === true) {
+        CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML += `<div class="_${Item_Class_NUM}">
+        <img src="Items_IMG/${item.img}" alt="${item.img}">
+        <div class="item_info">
+            <h2 class="item_Name">${item.item}</h2>
+            <div class="item_Price_NumberinCart">
+            <h2 class="item_Price" id="item_Price_${item.id}">$ ${item.price}</h2>
+            <h2 class="item_NumberInCart" id="item_NumberInCart_${item.id} recommended_item_NumberInCart_Item_${item.id}">0</h2>
+            </div>
+        </div>
+        <div class="Submit_Info">
+        <input type="number" min="1" max="100" oninput="number_Cramping(${item.id})" value=1 class="item_quantity_choosing" id="item_quantity_${item.id}">
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button" id="Add_To_Cart_Button_${item.id}">Add To Cart</button>
+        </div>
+        </div>`
+        } else {
         CHOSEN_FOOD_ITEM_OUTPUT_AREA.innerHTML += `<div class="_${Item_Class_NUM}">
         <img src="Items_IMG/${item.img}" alt="${item.img}">
         <div class="item_info">
@@ -302,9 +317,11 @@ function Menu_Item_Selecting_Desserts (){
         </div>
         <div class="Submit_Info">
         <input type="number" min="1" max="100" oninput="number_Cramping(${item.id})" value=1 class="item_quantity_choosing" id="item_quantity_${item.id}">
-        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button" id="Add_To_Cart_Button_${item.id}">Add To Cart</button>
+        <button value=${item.id} onclick="Add_To_Cart_Function(this.value)" class="Add_To_Cart_Button" id="Add_To_Cart_Button_${item.id} recommended_item_NumberInCart_Item_${item.id}">Add To Cart</button>
         </div>
         </div>`
+        }
+
         Item_Class_NUM = Item_Class_NUM + 1;
     }
 }
